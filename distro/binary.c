334ac13f3f62ef817bf2c4471d0907da9386b1a8
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2021-02-16 15:43:22 -0500

    Update binary.c

--- distro/binary.c
+++ distro/binary.c
@@ -5,24 +5,29 @@ bool search(int value, int values[], int start, int end);
 
 int main(void)
 {
+    // Define sorted array
     int values[] = {-1, 2, 3, 5, 7, 10, 15, 21, 32, 50};
-    
-    int value = get_int("Enter a number: );
-    
+
+    // Get input
+    int value = get_int("Enter a number: ");
+
+    // Define start and end values to begin searching
     int start = 0;
     int end = 9;
-    
+
+    // Check if input value is found
     if (search(value, values, start, end))
     {
         printf("Found!\n");
     }
     else
     {
-        printf("Not found!");
+        printf("Not found!\n");
     }
 }
 
 bool search(int value, int values[], int start, int end)
 {
     // TODO
+    return false;
 }
