a1496aadbac1ee6f0818cefafaa6dcb8ffbae4a7
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2022-06-15 08:28:10 -0400

    Create debug.c

---
+++ distro/debug.c
@@ -0,0 +1,14 @@
+// Become familiar wih C syntax
+// Learn to debug buggy code
+
+#include <cs50.h>
+
+int main(void)
+{
+    // Ask for your name and where live
+    name = get_string("What is your name: ")
+    location = get_string("Where do you live?: ")
+
+    // Say hello
+    print("Hello, %i, from %i!", name, location)
+}
